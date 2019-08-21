82e52048e2a7008e1aea2aa464b25698fcb0dbd3
Kevin-Dabrowski <kevin3.16@hotmail.com>
2019-08-21 18:25:36 -0400

    Update stock data.py

--- stock data.py
+++ stock data.py
@@ -6,7 +6,7 @@ import time                 #for time counter
 import urllib.request
 
 # prepares empty file with header
-open('data3.txt', 'w').close()       #Erase old fild
+open('data3.csv', 'w').close()       #Erase old fild
 
 header = [] * 100
 dfs = pd.read_html('https://ca.finance.yahoo.com/quote/AAB.TO/history?p=AAB.TO',header=0)
@@ -21,6 +21,7 @@ Column4 = ["-"] * (len(lists)-1)
 Column5 = ["-"] * (len(lists)-1)
 Column6 = ["-"] * (len(lists)-1)
 Column7 = ["-"] * (len(lists)-1)
+ListTotal = [Column1,Column2, Column3, Column4, Column5, Column6, Column7]
 print(str(lists[0]))
 for d in range(100):                #Manually typed in the number of rows because len(table) doesnt work
     Column1[d] = lists[d][0]
@@ -30,10 +31,16 @@ for d in range(100):                #Manually typed in the number of rows becaus
     Column5[d] = lists[d][4]
     Column6[d] = lists[d][5]
     Column7[d] = lists[d][6]
-    #print(Column1[d],Column2[d],Column3[d],Column4[d],Column5[d],Column6[d],Column7[d])
-f = open("data3.txt", "a")
-f.write(str(Column1[1]))
-f.write("\n")
+    #print(d,Column1[d],Column2[d],Column3[d],Column4[d],Column5[d],Column6[d],Column7[d])
+f = open("data3.csv", "a")
+f.write("bob")
+for d in range(7): 
+    with open('data3.csv', 'a') as filehandle:
+        filehandle.write('%s\n' % ListTotal[d])
+        f.write("\n")
+##f.write(str(lists[0]))
+##f.write("\n")
+##f.write("bob")
 
 #print(str(lists[1][0]))
 
@@ -42,4 +49,3 @@ f.write("\n")
 ##    df.to_csv(r'C:\Users\kevin\Desktop\python file exsports\web-scraper-master\data3.csv')
 ##    f = open("data3.csv", "a")
 ##    f.write(str(df)+"\n")
-
